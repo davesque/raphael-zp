c67e1974b0c392c06c9828e48e51c17eab21c3cb
David Sanders <dsanders@fusionbox.com>
2013-04-10 12:28:06 -0600

    Convert tabs to spaces

--- raphael-zp.js
+++ raphael-zp.js
@@ -38,215 +38,215 @@
  */
 
 (function() {
-	var initialized = false;
+  var initialized = false;
 
-	var opts = { zoom: true, pan: true, drag: true };
+  var opts = { zoom: true, pan: true, drag: true };
 
-	function init(paper) {
-		/**
-		 * Handler registration
-		 */
-		function setupHandlers(root) {
-			root.onmousedown = handleMouseDown;
-			root.onmousemove = handleMouseMove;
-			root.onmouseup = handleMouseUp;
-			//root.onmouseout = handleMouseUp; // Decomment this to stop the pan functionality when dragging out of the SVG element
+  function init(paper) {
+    /**
+     * Handler registration
+     */
+    function setupHandlers(root) {
+      root.onmousedown = handleMouseDown;
+      root.onmousemove = handleMouseMove;
+      root.onmouseup = handleMouseUp;
+      //root.onmouseout = handleMouseUp; // Decomment this to stop the pan functionality when dragging out of the SVG element
 
-			if(navigator.userAgent.toLowerCase().indexOf('webkit') >= 0)
-				window.addEventListener('mousewheel', handleMouseWheel, false); // Chrome/Safari
-			else
-				window.addEventListener('DOMMouseScroll', handleMouseWheel, false); // Others
-		}
+      if(navigator.userAgent.toLowerCase().indexOf('webkit') >= 0)
+        window.addEventListener('mousewheel', handleMouseWheel, false); // Chrome/Safari
+      else
+        window.addEventListener('DOMMouseScroll', handleMouseWheel, false); // Others
+    }
 
-		var root = paper.canvas;
+    var root = paper.canvas;
 
-		var g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
-		g.id = 'viewport';
+    var g = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    g.id = 'viewport';
 
-		root.appendChild(g);
-		paper.canvas = g;
+    root.appendChild(g);
+    paper.canvas = g;
 
-		var state = 'none', stateTarget, stateOrigin, stateTf;
+    var state = 'none', stateTarget, stateOrigin, stateTf;
 
-		setupHandlers(root);
+    setupHandlers(root);
 
-		initialized = true;
+    initialized = true;
 
-		/**
-		 * Instance an SVGPoint object with given event coordinates.
-		 */
-		function getEventPoint(evt) {
-			var p = root.createSVGPoint();
+    /**
+     * Instance an SVGPoint object with given event coordinates.
+     */
+    function getEventPoint(evt) {
+      var p = root.createSVGPoint();
 
-			p.x = evt.clientX;
-			p.y = evt.clientY;
+      p.x = evt.clientX;
+      p.y = evt.clientY;
 
-			return p;
-		}
+      return p;
+    }
 
-		/**
-		 * Sets the current transform matrix of an element.
-		 */
-		function setCTM(element, matrix) {
-			var s = "matrix(" + matrix.a + "," + matrix.b + "," + matrix.c + "," + matrix.d + "," + matrix.e + "," + matrix.f + ")";
+    /**
+     * Sets the current transform matrix of an element.
+     */
+    function setCTM(element, matrix) {
+      var s = "matrix(" + matrix.a + "," + matrix.b + "," + matrix.c + "," + matrix.d + "," + matrix.e + "," + matrix.f + ")";
 
-			element.setAttribute("transform", s);
-		}
+      element.setAttribute("transform", s);
+    }
 
-		/**
-		 * Dumps a matrix to a string (useful for debug).
-		 */
-		function dumpMatrix(matrix) {
-			var s = "[ " + matrix.a + ", " + matrix.c + ", " + matrix.e + "\n  " + matrix.b + ", " + matrix.d + ", " + matrix.f + "\n  0, 0, 1 ]";
+    /**
+     * Dumps a matrix to a string (useful for debug).
+     */
+    function dumpMatrix(matrix) {
+      var s = "[ " + matrix.a + ", " + matrix.c + ", " + matrix.e + "\n  " + matrix.b + ", " + matrix.d + ", " + matrix.f + "\n  0, 0, 1 ]";
 
-			return s;
-		}
+      return s;
+    }
 
-		/**
-		 * Sets attributes of an element.
-		 */
-		function setAttributes(element, attributes){
-			for (var i in attributes)
-				element.setAttributeNS(null, i, attributes[i]);
-		}
+    /**
+     * Sets attributes of an element.
+     */
+    function setAttributes(element, attributes){
+      for (var i in attributes)
+        element.setAttributeNS(null, i, attributes[i]);
+    }
 
-		/**
-		 * Handle mouse move event.
-		 */
-		function handleMouseWheel(evt) {
-			if (!opts.zoom) return;
+    /**
+     * Handle mouse move event.
+     */
+    function handleMouseWheel(evt) {
+      if (!opts.zoom) return;
 
-			if(evt.preventDefault)
-				evt.preventDefault();
+      if(evt.preventDefault)
+        evt.preventDefault();
 
-			evt.returnValue = false;
+      evt.returnValue = false;
 
-			var svgDoc = evt.target.ownerDocument;
+      var svgDoc = evt.target.ownerDocument;
 
-			var delta;
+      var delta;
 
-			if(evt.wheelDelta)
-				delta = evt.wheelDelta / 3600; // Chrome/Safari
-			else
-				delta = evt.detail / -90; // Mozilla
+      if(evt.wheelDelta)
+        delta = evt.wheelDelta / 3600; // Chrome/Safari
+      else
+        delta = evt.detail / -90; // Mozilla
 
-			var z = 1 + delta; // Zoom factor: 0.9/1.1
+      var z = 1 + delta; // Zoom factor: 0.9/1.1
 
-			var g = svgDoc.getElementById("viewport");
+      var g = svgDoc.getElementById("viewport");
 
-			var p = getEventPoint(evt);
+      var p = getEventPoint(evt);
 
-			p = p.matrixTransform(g.getCTM().inverse());
+      p = p.matrixTransform(g.getCTM().inverse());
 
-			// Compute new scale matrix in current mouse position
-			var k = root.createSVGMatrix().translate(p.x, p.y).scale(z).translate(-p.x, -p.y);
+      // Compute new scale matrix in current mouse position
+      var k = root.createSVGMatrix().translate(p.x, p.y).scale(z).translate(-p.x, -p.y);
 
-				setCTM(g, g.getCTM().multiply(k));
+        setCTM(g, g.getCTM().multiply(k));
 
-			if(typeof(stateTf) == "undefined")
-				stateTf = g.getCTM().inverse();
+      if(typeof(stateTf) == "undefined")
+        stateTf = g.getCTM().inverse();
 
-			stateTf = stateTf.multiply(k.inverse());
-		}
+      stateTf = stateTf.multiply(k.inverse());
+    }
 
-		/**
-		 * Handle mouse move event.
-		 */
-		function handleMouseMove(evt) {
+    /**
+     * Handle mouse move event.
+     */
+    function handleMouseMove(evt) {
       var p;
 
-			if(evt.preventDefault)
-				evt.preventDefault();
+      if(evt.preventDefault)
+        evt.preventDefault();
 
-			evt.returnValue = false;
+      evt.returnValue = false;
 
-			var svgDoc = evt.target.ownerDocument;
+      var svgDoc = evt.target.ownerDocument;
 
-			var g = svgDoc.getElementById("viewport");
+      var g = svgDoc.getElementById("viewport");
 
-			if(state == 'pan') {
-				// Pan mode
-				if (!opts.pan) return;
+      if(state == 'pan') {
+        // Pan mode
+        if (!opts.pan) return;
 
-				p = getEventPoint(evt).matrixTransform(stateTf);
+        p = getEventPoint(evt).matrixTransform(stateTf);
 
-				setCTM(g, stateTf.inverse().translate(p.x - stateOrigin.x, p.y - stateOrigin.y));
-			} else if(state == 'move') {
-				// Move mode
-				if (!opts.drag) return;
+        setCTM(g, stateTf.inverse().translate(p.x - stateOrigin.x, p.y - stateOrigin.y));
+      } else if(state == 'move') {
+        // Move mode
+        if (!opts.drag) return;
 
-				p = getEventPoint(evt).matrixTransform(g.getCTM().inverse());
+        p = getEventPoint(evt).matrixTransform(g.getCTM().inverse());
 
-				setCTM(stateTarget, root.createSVGMatrix().translate(p.x - stateOrigin.x, p.y - stateOrigin.y).multiply(g.getCTM().inverse()).multiply(stateTarget.getCTM()));
+        setCTM(stateTarget, root.createSVGMatrix().translate(p.x - stateOrigin.x, p.y - stateOrigin.y).multiply(g.getCTM().inverse()).multiply(stateTarget.getCTM()));
 
-				stateOrigin = p;
-			}
-		}
+        stateOrigin = p;
+      }
+    }
 
-		/**
-		 * Handle click event.
-		 */
-		function handleMouseDown(evt) {
-			if(evt.preventDefault)
-				evt.preventDefault();
+    /**
+     * Handle click event.
+     */
+    function handleMouseDown(evt) {
+      if(evt.preventDefault)
+        evt.preventDefault();
 
-			evt.returnValue = false;
+      evt.returnValue = false;
 
-			var svgDoc = evt.target.ownerDocument;
+      var svgDoc = evt.target.ownerDocument;
 
-			var g = svgDoc.getElementById("viewport");
+      var g = svgDoc.getElementById("viewport");
 
-			if(evt.target.tagName == "svg") {
-				// Pan mode
-				if (!opts.pan) return;
+      if(evt.target.tagName == "svg") {
+        // Pan mode
+        if (!opts.pan) return;
 
-				state = 'pan';
+        state = 'pan';
 
-				stateTf = g.getCTM().inverse();
+        stateTf = g.getCTM().inverse();
 
-				stateOrigin = getEventPoint(evt).matrixTransform(stateTf);
-			} else {
-				// Move mode
-				if (!opts.drag || evt.target.draggable === false) return;
+        stateOrigin = getEventPoint(evt).matrixTransform(stateTf);
+      } else {
+        // Move mode
+        if (!opts.drag || evt.target.draggable === false) return;
 
-				state = 'move';
+        state = 'move';
 
-				stateTarget = evt.target;
+        stateTarget = evt.target;
 
-				stateTf = g.getCTM().inverse();
+        stateTf = g.getCTM().inverse();
 
-				stateOrigin = getEventPoint(evt).matrixTransform(stateTf);
-			}
-		}
+        stateOrigin = getEventPoint(evt).matrixTransform(stateTf);
+      }
+    }
 
-		/**
-		 * Handle mouse button release event.
-		 */
-		function handleMouseUp(evt) {
-			if(evt.preventDefault)
-				evt.preventDefault();
+    /**
+     * Handle mouse button release event.
+     */
+    function handleMouseUp(evt) {
+      if(evt.preventDefault)
+        evt.preventDefault();
 
-			evt.returnValue = false;
+      evt.returnValue = false;
 
-			var svgDoc = evt.target.ownerDocument;
+      var svgDoc = evt.target.ownerDocument;
 
-			if((state == 'pan' && opts.pan) || (state == 'move' && opts.drag)) {
-				// Quit pan mode
-				state = '';
-			}
-		}
-	}
+      if((state == 'pan' && opts.pan) || (state == 'move' && opts.drag)) {
+        // Quit pan mode
+        state = '';
+      }
+    }
+  }
 
-	Raphael.fn.ZPD = function(o) {
-		if (o) {
-			for (var key in o) {
-				if (opts[key] !== undefined) {
-					opts[key] = o[key];
-				}
-			}
-		}
+  Raphael.fn.ZPD = function(o) {
+    if (o) {
+      for (var key in o) {
+        if (opts[key] !== undefined) {
+          opts[key] = o[key];
+        }
+      }
+    }
 
-		if (!initialized) init(this);
-		return this;
+    if (!initialized) init(this);
+    return this;
   };
 })();
